3e3f873ae11991135cee6259d0c2a25dfaeb506a
Gabriel Brandão <gabrielbrandaosales@hotmail.com>
2023-09-26 15:19:40 -0300

    feat: intro done

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -4,20 +4,28 @@ import style from './style.module.scss';
 
 const Intro = () => {
   return (
-    <section className={style.conteudo}>
+    <section className={style.content}>
       <div id={style.mask}>
-        <Image
+        {/* <Image
           className={style.introImage}
           alt={'profile picture'}
           src={'/principal-image.jpg'}
           width={450}
           height={925}
           priority
-        />
+        /> */}
       </div>
-      <div>
+      <div className={style.intro}>
+        <Image
+          className={style.detail}
+          alt={'profile picture'}
+          src={'/detalhe.svg'}
+          width={100}
+          height={100}
+          priority
+        />
         <h1>Desenvolvedor Full Stack</h1>
-        <p>Localizado em São Paulo</p>
+        <p>Localizado em São Paulo 🏖️</p>
       </div>
     </section>
   );
