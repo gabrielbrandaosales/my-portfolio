6e5270430fe340f67348bab155ab6bb407b7403d
Gabriel Brandão <gabrielbrandaosales@hotmail.com>
2023-10-02 22:47:52 -0300

    feat: new component card education

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './style.module.scss';
+import CardEducation from '../Card/CardEducation';
 
 const Education = () => {
   return (
@@ -13,37 +14,12 @@ const Education = () => {
           exterior em UX Design. Além disso me mantenho sempre atualizado com
           cursos intensivos online.
         </p>
-        <div className={style.list}>
-          <span className={style.year}>2023</span>
-          <h2 aria-label="Company">GBM Tech & Control</h2>
-          <h2 aria-label="Position">Developer Jr</h2>
-          <p className={style.description} aria-label="Position description">
-            Desenvolvo pequenos projetos como o Bikcraft utilizando apenas HTML,
-            CSS e JavaScript. Para aplicativos web como a rede social Dogs.
-          </p>
-          <div className={style.tags} aria-label="technologies">
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JavaScript</p>
-            <p>Next</p>
-            <p>Node</p>
-          </div>
-        </div>
-        <div className={style.list}>
-          <span className={style.year}>2021-22</span>
-          <h2 aria-label="Company">BDS Data Solutions</h2>
-          <h2 aria-label="Position">Developer</h2>
-          <p className={style.description} aria-label="Position description">
-            Desenvolvo pequenos projetos como o Bikcraft utilizando apenas HTML,
-            CSS e JavaScript. Para aplicativos web como a rede social Dogs.
-          </p>
-          <div className={style.tags} aria-label="technologies">
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JavaScript</p>
-            <p>React</p>
-            <p>PostgreSQL</p>
-          </div>
+        <div className={style.cardGroup}>
+          <CardEducation
+            title="Tecnólogo"
+            name="Análise e Desenvolvimento de Sistemas"
+            foundation="USJT"
+          />
         </div>
       </article>
     </section>
