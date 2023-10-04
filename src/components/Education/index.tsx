2a23442c66ef0fb2a93a344b50462d4c04c66c42
Gabriel Brandão <gabrielbrandaosales@hotmail.com>
2023-10-03 21:59:11 -0300

    fix: adjust background section color

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -9,29 +9,31 @@ var courses: [
 const Education = () => {
   return (
     <section className={style.container} id="experience">
-      <span>
-        <h1 className={style.title}>formação</h1>
-      </span>
-      <article>
-        <p className={style.experience} aria-label="Experience description">
-          Minha mais recente experiência acadêmica foi o mestrado 🎓 que fiz no
-          exterior em UX Design. Além disso me mantenho sempre atualizado com
-          cursos intensivos online.
-        </p>
-        <div className={style.cardGroup}>
-          <CardEducation
-            title="Tecnólogo"
-            name="Análise e Desenvolvimento de Sistemas"
-            foundation="USJT"
+      <div className={style.content}>
+        <span>
+          <h1 className={style.title}>formação</h1>
+        </span>
+        <article>
+          <p className={style.experience} aria-label="Experience description">
+            Minha mais recente experiência acadêmica foi o mestrado 🎓 que fiz
+            no exterior em UX Design. Além disso me mantenho sempre atualizado
+            com cursos intensivos online.
+          </p>
+          <div className={style.cardGroup}>
+            <CardEducation
+              title="Tecnólogo"
+              name="Análise e Desenvolvimento de Sistemas"
+              foundation="USJT"
+            />
+          </div>
+          <CoursesSection
+            courses={[
+              { name: 'React Completo', time: '36h' },
+              { name: 'HTML e CSS para iniciantes', time: '46h' },
+            ]}
           />
-        </div>
-        <CoursesSection
-          courses={[
-            { name: 'React Completo', time: '36h' },
-            { name: 'HTML e CSS para iniciantes', time: '46h' },
-          ]}
-        />
-      </article>
+        </article>
+      </div>
     </section>
   );
 };
