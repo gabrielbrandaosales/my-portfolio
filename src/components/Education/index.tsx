3e79b026893df0a3792dda2fdf6b03b4271c3365
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2023-10-10 13:31:30 -0300

    feat: add course

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -33,6 +33,7 @@ const Education = () => {
           <CoursesSection
             courses={[
               { name: 'React Completo', time: '36h' },
+              { name: 'JavaScript Completo ES6', time: '74h' },
               { name: 'HTML e CSS para iniciantes', time: '46h' },
             ]}
           />
