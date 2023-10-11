f25935f30356b4c47e9a911d0b6e2a55bde59a86
Gabriel Brandão <50925360+gabrielbrandaosales@users.noreply.github.com>
2023-10-11 20:52:44 -0300

    Feature/projects section (#5)
    * feat: initial section development
    
    * feat: final styling project card

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import style from './style.module.scss';
+import CardProject from '../Card/CardProject';
 
 const Projects = () => {
-  return <div>Project</div>;
+  return (
+    <section className={style.container} id="projects">
+      <div className={style.content}>
+        <span>
+          <h1 className={style.title}>Projetos</h1>
+        </span>
+        <article>
+          <p className={style.subtitle}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.{' '}
+          </p>
+          <div className={style.feedProjects}>
+            <CardProject />
+            <CardProject />
+            <CardProject />
+            <CardProject />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
