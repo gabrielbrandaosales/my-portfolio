5feee1623d6acf044ce8c737d3ebcd6c5ebfbe8e
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2023-10-15 02:54:33 -0300

    feat: add api github and styles

--- src/components/Card/CardProject/index.tsx
+++ src/components/Card/CardProject/index.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import style from './style.module.scss';
 import Image from 'next/image';
+import { Repos } from '@/@types/repos';
 
-const CardProject = () => {
+interface Props {
+  data: Repos;
+}
+
+const CardProject: React.FC<Props> = ({ data }) => {
+  console.log(data);
+
   return (
     <div className={style.Card}>
-      <div className={style.verticalFlex}>
-        <div>
+      <div className={style.content}>
+        <a href={data.homepage} target="_blank">
           <p className={style.project}>PROJETO</p>
-          <h1 className={style.title}>name</h1>
-        </div>
-        <p className={style.time}>time</p>
+          <h1 className={style.title}>{data.name}</h1>
+        </a>
+        <p className={style.time}>
+          {data.topics.map((topic, index) => (
+            <span className={style.topic} key={index}>
+              {topic}
+            </span>
+          ))}
+        </p>
       </div>
 
-      <Image
+      {/* <Image
         className={style.image}
-        src="blob:https://vercel.com/cd7f074c-b885-4819-994a-d1fe670ff3fc"
+        src="#"
         width={241}
         height={150}
         alt="image"
-      />
+      /> */}
     </div>
   );
 };
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './style.module.scss';
+import { api } from '../../lib/axios';
 import CardProject from '../Card/CardProject';
+import { Repos } from '@/@types/repos';
 
 const Projects = () => {
+  const [repos, setRepos] = useState<Repos[]>([] as Repos[]);
+
+  useEffect(() => {
+    api
+      .get<Repos[]>('/users/gabrielbrandaosales/repos')
+      .then((response) => setRepos(response.data));
+  }, []);
   return (
     <section className={style.container} id="projects">
       <div className={style.content}>
@@ -15,10 +24,14 @@ const Projects = () => {
             industry.{' '}
           </p>
           <div className={style.feedProjects}>
-            <CardProject />
-            <CardProject />
-            <CardProject />
-            <CardProject />
+            {repos
+              .filter(
+                (repo) =>
+                  repo.fork == false && repo.homepage && repo.has_wiki === true,
+              )
+              .map((repo) => (
+                <CardProject data={repo} key={repo.id} />
+              ))}
           </div>
         </article>
       </div>
